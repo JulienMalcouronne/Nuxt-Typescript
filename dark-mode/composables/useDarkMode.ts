4ed4a8ecb8611c6814ca058fbc6e2a51b542ed3f
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-08-31 22:41:53 +0200

    train composable

---
+++ dark-mode/composables/useDarkMode.ts
@@ -0,0 +1,16 @@
+const useDarkMode = () => {
+  const isDarkMode = useState("darkMode", () => false);
+
+  const toggleDarkMode = () => {
+    isDarkMode.value = !isDarkMode.value;
+  }
+
+  return {
+    isDarkMode,
+    toggleDarkMode,
+  };
+
+
+};
+
+export default useDarkMode;
